79cad2b3045b16f5406393631ac29b042f0c86ff
Dragan Stojilkovic <149414184+Gagasr10@users.noreply.github.com>
2024-06-10 19:54:24 +0200

    Add files via upload

---
+++ src/cubes/driver/MyWebDriver.java
@@ -0,0 +1,43 @@
+package cubes.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyWebDriver {
+	
+		
+	private static MyWebDriver driver;
+		
+
+	private MyWebDriver() {
+		
+	}
+	
+	public static MyWebDriver getInstance() {
+		if (driver == null) {
+			driver = new MyWebDriver();
+		}
+		return driver;
+	}
+
+		
+	public WebDriver getDriver(String browser) {
+		
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\chromedriver.exe");
+			
+			return new ChromeDriver();
+		}
+		else {
+			System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\chromedriver.exe");
+			
+			return new ChromeDriver();
+			
+		}
+		
+		
+	}
+	
+	 	
+	
+}
